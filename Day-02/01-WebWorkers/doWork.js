c2afe8ac7a852162997e1b4dd8c622316d0ccddd
timbertinker <forest310125@gmail.com>
2015-01-09 11:01:14 +0530

    Progress Update - Solution

--- Day-02/01-WebWorkers/doWork.js
+++ Day-02/01-WebWorkers/doWork.js
@@ -1,11 +1,20 @@
 function doWork(){
-	for(var i=0;i<10000;i++)
+	for(var i=0;i<10000;i++){
 		for(var j=0;j<10000;j++)
 			for(var k=0;k<100;k++){}
+		if (i % 100 === 0){
+			self.postMessage({
+				type : "progress",
+				percentCompleted : (i / 10000) * 100
+			});
+		}
+	}
 }
 self.addEventListener("message", function(msgEvtArg){
 	if (msgEvtArg.data === "start"){
 		doWork();
-		self.postMessage("completed");
+		self.postMessage({
+			type : "completed"
+		});
 	}
 });
